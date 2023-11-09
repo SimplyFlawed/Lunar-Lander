ddf012b59b0d51789d745dfa254e39e6388c01a4
SimplyFlawed <72576623+SimplyFlawed@users.noreply.github.com>
2023-11-08 23:52:29 -0500

    Added header text

--- main.cpp
+++ main.cpp
@@ -1,3 +1,13 @@
+/**
+* Author: Raymond Lin
+* Assignment: Lunar Lander
+* Date due: 2023-11-08, 11:59pm
+* I pledge that I have completed this assignment without
+* collaborating with anyone else, in conformance with the
+* NYU School of Engineering Policies and Procedures on
+* Academic Misconduct.
+**/
+
 #define LOG(argument) std::cout << argument << '\n'
 #define STB_IMAGE_IMPLEMENTATION
 #define GL_SILENCE_DEPRECATION
